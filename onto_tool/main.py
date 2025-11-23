37822ad11d710ec99f50e0a9f487d372f5fffec2
AleLocci <alelocci@ime.usp.br>
2025-11-23 18:14:20 +0000

    Teste de criação de uma ontologia.

--- onto_tool/main.py
+++ onto_tool/main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+from owlready2 import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 1. Criar uma nova ontologia
+onto = get_ontology("http://example.org/onto.owl")
 
+with onto:
+    # 2. Definir classes
+    class Pessoa(Thing):
+        pass
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    class Estudante(Pessoa):
+        pass
 
+    class Professor(Pessoa):
+        pass
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # 3. Definir propriedades
+    class ensina(ObjectProperty):
+        domain = [Professor]
+        range = [Estudante]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    class idade(DataProperty):
+        domain = [Pessoa]
+        range = [int]
+
+# 4. Criar instâncias
+joao = Professor("Joao")
+maria = Estudante("Maria")
+
+joao.ensina.append(maria)
+joao.idade = [40]
+maria.idade = [22]
+
+# 5. Salvar a ontologia em arquivo OWL
+onto.save(file="onto.owl", format="rdfxml")
+
+# 6. Consultar
+print(f"{joao.name} ensina {joao.ensina[0].name}")
+print(f"Idade de {maria.name}: {maria.idade}")
