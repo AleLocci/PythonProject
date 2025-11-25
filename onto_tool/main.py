5e3325069421a9d23835f1414a9a50584e6ef562
AleLocci <alelocci@ime.usp.br>
2025-11-25 09:07:04 +0000

    Teste de criação de uma ontologia.

--- onto_tool/main.py
+++ onto_tool/main.py
@@ -1,39 +1,12 @@
+import rdflib
+from pyshacl import validate
 from owlready2 import *
 
-# 1. Criar uma nova ontologia
-onto = get_ontology("http://example.org/onto.owl")
 
-with onto:
-    # 2. Definir classes
-    class Pessoa(Thing):
-        pass
+onto = get_ontology("https://protege.stanford.edu/ontologies/pizza/pizza.owl").load()
 
-    class Estudante(Pessoa):
-        pass
-
-    class Professor(Pessoa):
-        pass
-
-    # 3. Definir propriedades
-    class ensina(ObjectProperty):
-        domain = [Professor]
-        range = [Estudante]
-
-    class idade(DataProperty):
-        domain = [Pessoa]
-        range = [int]
-
-# 4. Criar instâncias
-joao = Professor("Joao")
-maria = Estudante("Maria")
-
-joao.ensina.append(maria)
-joao.idade = [40]
-maria.idade = [22]
-
-# 5. Salvar a ontologia em arquivo OWL
-onto.save(file="onto.owl", format="rdfxml")
-
-# 6. Consultar
-print(f"{joao.name} ensina {joao.ensina[0].name}")
-print(f"Idade de {maria.name}: {maria.idade}")
+cls = []
+cls = onto.classes()
+ind= onto.individuals()
+print(list(cls))
+print(list(ind))
